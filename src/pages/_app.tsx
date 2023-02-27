d5197f1a11de3e47876424582a9b1798f8565903
abdul <malik301002@gmail.com>
2023-02-28 00:15:08 +0700

    feat(pages): apply inter font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { type AppType } from 'next/app';
+import { Inter } from '@next/font/google';
 
 import '@/styles/globals.css';
 
+const inter = Inter({ subsets: ['latin'] });
+
 const MyApp: AppType = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <style jsx global>
+      {`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}
+    </style>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
