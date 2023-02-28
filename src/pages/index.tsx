b92fff4fcef49b6c1aacff395a996df1866d627b
abdul <malik301002@gmail.com>
2023-02-28 11:57:40 +0700

    feat: create SearchBar component and add it to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,34 @@ import Balancer from 'react-wrap-balancer';
 
 import { cn } from '@/lib/utils';
 import Layout from '@/components/organisms/Layout';
+import SearchBar from '@/components/molecules/SearchBar';
 
 const Home: NextPage = () => (
   <Layout>
-    <h1 className='mb-3 text-center text-xl'>🍱 🍜 🥪</h1>
-    <h1
+    <div
       className={cn(
-        'text-center text-3xl',
-        'leading-relaxed underline decoration-cream-200 decoration-wavy underline-offset-8',
+        'border-b-2 border-dashed border-mine-500 sm:border-x-2',
+        'px-3 py-16 sm:rounded-b-3xl',
       )}
     >
-      <Balancer>
-        Get your <span className='text-cream-200'>delicious</span> meal{' '}
-        <span className='text-cream-200'>recipes</span> here!
-      </Balancer>
-    </h1>
+      <h1 className='mb-3 text-center text-xl'>🍱 🍜 🥪</h1>
+      <h1
+        className={cn(
+          'text-center text-2xl sm:text-3xl',
+          'leading-relaxed decoration-cream-200 decoration-wavy',
+          'underline underline-offset-2 md:underline-offset-4 lg:underline-offset-8',
+        )}
+      >
+        <Balancer>
+          Get your <span className='text-cream-200'>delicious</span> meal{' '}
+          <span className='text-cream-200'>recipes</span> here!
+        </Balancer>
+      </h1>
+    </div>
+
+    <section className='mt-10'>
+      <SearchBar />
+    </section>
   </Layout>
 );
 
