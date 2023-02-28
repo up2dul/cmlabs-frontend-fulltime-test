9b7891b18f2bc04cba34001d79c3658769b72dfa
abdul <malik301002@gmail.com>
2023-03-01 01:21:49 +0700

    feat(utils): `textToSlug` and `slugToNormal` functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,4 +7,27 @@ function cn(...inputs: ClassValue[]) {
 }
 //#endregion cn
 
-export { cn };
+//#region textToSlug
+function textToSlug(str: string) {
+  return str.toLowerCase().replace(/ /g, '-');
+}
+//#endregion textToSlug
+
+//#region slugToNormal
+function capitalizeFirstLetter(str: string) {
+  const firstLetter = str.charAt(0).toUpperCase();
+  const result = [...str];
+  result[0] = firstLetter;
+
+  return result.join('');
+}
+
+function slugToNormal(slug: string) {
+  return slug
+    .split('-')
+    .map((str) => capitalizeFirstLetter(str))
+    .join(' ');
+}
+//#endregion slugToNormal
+
+export { cn, textToSlug, slugToNormal };
