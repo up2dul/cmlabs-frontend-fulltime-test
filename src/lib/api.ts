5883590df991f31022019eb6a81d64beac622b1e
abdul <malik301002@gmail.com>
2023-03-01 21:53:02 +0700

    feat: create `getAllMeals`, `getMeal` api method and MealDe--
    --tail type

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { type AxiosInstance } from 'axios';
 
-import type { Meal, Ingredient } from './types';
+import type { Meal, Ingredient, MealDetail } from './types';
 
 const instance = axios.create({
   baseURL: 'https://www.themealdb.com/api/json/v1/1/',
@@ -13,6 +13,15 @@ function api(axios: AxiosInstance) {
     },
     getIngredient: (query: string) => {
       return axios.get<{ meals: Meal[] }>(`filter.php?i=${query}`);
+    },
+    getAllMeals: () => {
+      return axios.get<{ meals: MealDetail[] }>('search.php?s=');
+    },
+    getMeal: async (id: string) => {
+      const res = await axios.get<{ meals: MealDetail[] }>(
+        `lookup.php?i=${id}`,
+      );
+      return res.data.meals[0];
     },
   };
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,8 +5,32 @@ export type Meal = {
 };
 
 export type MealDetail = Meal & {
-  strCategory: string;
-  // ...
+  strInstructions: string;
+  strYoutube: string;
+  //#region strIngredient
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strIngredient9: string;
+  strIngredient10: string;
+  //#endregion strIngredient
+  //#region strMeasure
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6: string;
+  strMeasure7: string;
+  strMeasure8: string;
+  strMeasure9: string;
+  strMeasure10: string;
+  //#endregion strMeasure
 };
 
 export type Ingredient = {
