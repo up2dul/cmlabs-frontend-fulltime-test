17e10444c0274131a261bc23e70f7c2bbf9cfcf6
abdul <malik301002@gmail.com>
2023-03-02 09:25:18 +0700

    feat(utils): `mergeIngredientsMeasures` function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import clsx, { type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+import { type MealDetail } from './types';
+
 //#region cn
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -30,4 +32,24 @@ function slugToNormal(slug: string) {
 }
 //#endregion slugToNormal
 
-export { cn, textToSlug, slugToNormal };
+//#region mergeIngredientsMeasures
+function mergeIngredientsMeasures(data: MealDetail) {
+  const ingredients = Object.entries(data || [])
+    .filter((ing) => ing[0].includes('strIngredient'))
+    .map((ing) => ing[1])
+    .filter((ing) => ing !== '');
+
+  const measures = Object.entries(data || [])
+    .filter((measure) => measure[0].includes('strMeasure'))
+    .map((measure) => measure[1])
+    .filter((measure) => measure !== '');
+
+  return ingredients.map((ingredient, idx) => ({
+    key: `${ingredient}-${measures[idx] as string}`,
+    name: ingredient,
+    measure: measures[idx],
+  }));
+}
+//#endregion mergeIngredientsMeasures
+
+export { cn, textToSlug, slugToNormal, mergeIngredientsMeasures };
